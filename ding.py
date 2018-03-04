4293825990a99ad59565481b31fbec23ccc74267
Valentin Deville <contact@valentin-deville.eu>
2018-03-04 14:15:15 +0100

    Use pygame instead of mplayer and subprocess

--- ding.py
+++ ding.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 import RPi.GPIO as GPIO
-import subprocess
 from os import listdir
 from os.path import isfile, join, dirname, realpath
+from pygame import mixer
 
 SCRIPT_PATH = dirname(realpath(__file__))
 SONG_PATH = SCRIPT_PATH + "/songs/"
@@ -14,7 +14,7 @@ GPIO.setup(GPIO_SIGNAL, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 song_list = [f for f in listdir(SONG_PATH) if isfile(join(SONG_PATH, f))]
 current_index = 0
-
+mixer.init()
 
 def update_index():
     global current_index
@@ -28,8 +28,10 @@ def play_next_song():
     song = SONG_PATH + song_list[current_index]
     print("Index: " + str(current_index))
     print("Playing: " + song)
-    player = subprocess.Popen(['mplayer', song], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    player.communicate()
+    player = mixer.Sound(file=song)
+    player.play()
+    while mixer.get_busy():
+        pass
 
     update_index()
 
