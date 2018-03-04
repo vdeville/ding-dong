68a097a4a2e7ea1d8019e8d1a03e93e8810e941b
Valentin Deville <contact@valentin-deville.eu>
2018-03-04 13:52:04 +0100

    Initial commit

---
+++ ding.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import RPi.GPIO as GPIO
+import subprocess
+from os import listdir
+from os.path import isfile, join, dirname, realpath
+
+SCRIPT_PATH = dirname(realpath(__file__))
+SONG_PATH = SCRIPT_PATH + "/songs/"
+GPIO_SIGNAL = 7
+BOUNCE_TIME = 200
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(GPIO_SIGNAL, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+song_list = [f for f in listdir(SONG_PATH) if isfile(join(SONG_PATH, f))]
+current_index = 0
+
+
+def update_index():
+    global current_index
+    if current_index >= len(song_list) - 1:
+        current_index = 0
+    else:
+        current_index += 1
+
+
+def play_next_song():
+    song = SONG_PATH + song_list[current_index]
+    print("Index: " + str(current_index))
+    print("Playing: " + song)
+    player = subprocess.Popen(['mplayer', song], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    player.communicate()
+
+    update_index()
+
+
+def launch_action():
+    play_next_song()
+
+
+def callback_gpio(channel):
+    if GPIO.input(GPIO_SIGNAL):
+        print("DING!")
+        # ACTION
+        launch_action()
+
+
+if __name__ == '__main__':
+    GPIO.add_event_detect(GPIO_SIGNAL, GPIO.RISING, callback=callback_gpio, bouncetime=BOUNCE_TIME)
+    while True:
+        pass
